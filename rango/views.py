bc9aef59f1e12e746158e806b5fc31f099c730d1
“Aiden <2747612l@student.gla.ac.uk>
2024-02-05 18:28:51 +0000

    updated index template

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 # Create your views here.
 def index(request):
@@ -16,3 +16,17 @@ def about(request):
     # context_dict = {}
     return render(request, 'rango/about.html')
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
