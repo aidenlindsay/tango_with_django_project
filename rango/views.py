3f1491b237d50b4da344f2278088f0252264ee5d
“Aiden <2747612l@student.gla.ac.uk>
2024-02-06 20:20:16 +0000

    Cookie saved

--- rango/views.py
+++ rango/views.py
@@ -18,12 +18,19 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
+    request.session.set_test_cookie()
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     # context_dict = {}
     print(request.method)
     print(request.user)
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     return render(request, 'rango/about.html')
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
